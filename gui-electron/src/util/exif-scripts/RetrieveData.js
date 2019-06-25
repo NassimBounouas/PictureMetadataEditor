91484bdd8d33287ec50a315307a77625e9f39160
Nassim Bounouas <nassim.bounouas@gmail.com>
2019-06-25 12:08:57 +0200

    Intersection and metadata collect

--- gui-electron/src/util/exif-scripts/RetrieveData.js
+++ gui-electron/src/util/exif-scripts/RetrieveData.js
@@ -3,31 +3,48 @@
 
 import { ExiftoolProcess } from "node-exiftool";
 import { exiftoolBin } from "dist-exiftool";
+import { readdirSync } from "fs";
 
 export function CopyMetadata(from, to) {
-    console.log('from :' + from);
-    console.log('to :' + to);
+    from = String(from);
+    to = String(to);
     
-    var ep = new ExiftoolProcess(exiftoolBin)
-    ep.open()
-        .then((pid) => console.log('Started exiftool process %s', pid))
-        .then(() => ep.readMetadata(from + '/pierre.jpg', ['GPSLatitude', 'GPSLongitude', 'DateTimeOriginal']))
-        .then((result, error) => {
-            if (result != undefined) {
-                console.log(result.data);
-                console.log(result.data.length);
-                if (result.data.length > 0) {
-                    console.log(result.data[0]);
-                    console.log(Object.keys(result.data[0]));
+    var srcFolder, destFolder;
+
+    try {
+        srcFolder = readdirSync(from);
+        destFolder = readdirSync(to);
+    } catch (err) {
+        console.log(err);
+    }
+
+    var intersection = srcFolder.filter(v => destFolder.includes(v));
+    
+    console.log('intersection : ' + intersection);
+
+    intersection.forEach( filename => {
+        var ep = new ExiftoolProcess(exiftoolBin);
+        var metadata = {};
+        ep.open()
+            .then(() => ep.readMetadata(from + '/' + filename, ['GPSLatitude -n', 'GPSLongitude -n', 'DateTimeOriginal']))
+            .then((result, error) => {
+                if (result != undefined) {
+                    if (result.data.length > 0) {
+                        console.log(result.data[0]);
+                        Object.keys(result.data[0]).forEach(key => {
+                            metadata[key] = result.data[0][key]
+                        })
+                    }
                 }
-            }
-            if (error != undefined) {
-                console.log(error);
-            }
-        })
-        .then(() => ep.readMetadata(to + '/pierre.jpg', ['-File:all']))
-        .then(console.log, console.error)
-        .then(() => ep.close())
-        .then(() => console.log('Closed exiftool'))
-        .catch(console.error)
+                if (error != undefined) {
+                    console.log(error);
+                }
+                console.log('metadata : ' + JSON.stringify(metadata));
+            })
+            .then(() => ep.readMetadata(to + '/pierre.jpg', ['-File:all']))
+            .then(console.log, console.error)
+            .then(() => ep.close())
+            .then(() => console.log('Closed exiftool'))
+            .catch(console.error)
+    });
 }
